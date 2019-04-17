c805a2625880a8b6d150aab357ce5a9198e80076
OksanaShipka <oks_sh@ukr.net>
2019-04-17 21:43:34 +0300

    add cte queries

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,7 +1,42 @@
 USE [labor_sql];
 GO
 
-/*Task 1*/
+/*Task 4*/
+;WITH first_level_regions(region_id, place_id, name, PlaceLevel) AS 
+(
+	SELECT region_id, id, name, PlaceLevel=-1
+	FROM geography
+	WHERE name = 'Ivano-Frankivsk'
+UNION ALL
+	SELECT g.region_id, g.id, g.name, f.PlaceLevel+1
+	FROM geography g INNER JOIN first_level_regions f
+
+ON g.region_id = f.place_id
+)
+SELECT region_id, place_id, name, PlaceLevel
+FROM first_level_regions
+WHERE PlaceLevel >= 0
+go
+
+/*Task 5*/
+; WITH RowCTE(row_no) AS
+(
+	SELECT row_no = 1
+	UNION ALL
+	SELECT row_no + 1 FROM RowCTE WHERE row_no < 10000
+)
+SELECT * FROM RowCTE
+option(maxrecursion 10000)
+
+/*Task 6*/
+; WITH RowCTE(row_no) AS
+(
+	SELECT row_no = 1
+	UNION ALL
+	SELECT row_no + 1 FROM RowCTE WHERE row_no < 100000
+)
+SELECT * FROM RowCTE
+option(maxrecursion 100000)
 
 /*Task 8*/
 
@@ -168,7 +203,150 @@ LEFT JOIN laptop ON product.model = laptop.model
 LEFT JOIN printer ON product.model = printer.model
 GROUP BY maker
 
-/*Task 32*/
+/* Task 25 */
+
+SELECT DISTINCT
+	maker,
+	CASE WHEN total_pc > 0 THEN CONCAT('yes(', total_pc, ')') ELSE 'no' END AS pc,
+	CASE WHEN total_laptop > 0 THEN CONCAT('yes(', total_laptop, ')') ELSE 'no' END AS laptop,
+	CASE WHEN total_printer > 0 THEN CONCAT('yes(', total_printer, ')') ELSE 'no' END AS printers
+FROM product AS makers
+CROSS APPLY
+(SELECT COUNT(*) AS total_pc
+	FROM pc INNER JOIN product
+	ON pc.model = product.model
+	WHERE product.maker = makers.maker) as pcs
+CROSS APPLY
+(SELECT COUNT(*) AS total_laptop
+	FROM laptop INNER JOIN product
+	ON laptop.model = product.model
+	WHERE product.maker = makers.maker) as laptops
+CROSS APPLY
+(SELECT COUNT(*) AS total_printer
+	FROM printer INNER JOIN product
+	ON printer.model = product.model
+	WHERE product.maker = makers.maker) as printers
+
+/* Task 26 */ 
+
+SELECT DISTINCT
+	points.point,
+	dates.[date],
+	outcomes_total.[out],
+	incomes_total.inc FROM
+(
+SELECT point from outcome_o
+UNION SELECT point from income_o
+) as points
+CROSS APPLY
+(
+SELECT date from outcome_o
+UNION SELECT date from income_o
+) as dates
+LEFT JOIN 
+	(SELECT [out], [date], point
+		FROM outcome_o) AS outcomes_total
+		ON outcomes_total.[date] = dates.[date]
+			AND outcomes_total.point = points.point
+LEFT JOIN
+	(SELECT inc, [date], point
+		FROM income_o) AS incomes_total
+		ON incomes_total.[date] = dates.[date]
+			AND incomes_total.point = points.point
+WHERE
+	outcomes_total.[out] IS NOT NULL
+	OR incomes_total.inc IS NOT NULL
+
+/* Task 27 */ 
+
+SELECT [name], numGuns, bore, displacement, [type], country, launched, [class]
+FROM
+(
+SELECT 
+	ships.[name],
+	classes.numGuns,
+	classes.bore,
+	classes.displacement,
+	classes.[type],
+	classes.country,
+	ships.launched,
+	classes.class,
+		CASE WHEN numGuns = 8 THEN 1 ELSE 0 END
+		+ CASE WHEN bore = 15 THEN 1 ELSE 0 END
+		+ CASE WHEN displacement = 32000 THEN 1 ELSE 0 END
+		+ CASE WHEN [type] = 'bb' THEN 1 ELSE 0 END
+		+ CASE WHEN country = 'USA' THEN 1 ELSE 0 END
+		+ CASE WHEN launched = 1915 THEN 1 ELSE 0 END
+		+ CASE WHEN classes.class = 'Kongo' THEN 1 ELSE 0 END AS [validation]
+FROM ships
+INNER JOIN classes
+ON ships.class = classes.class)
+AS result
+WHERE [validation] >= 4
+
+/* Task 28 */ 
+
+SELECT 
+	point,
+	left_side.[date],
+	CASE
+		WHEN once_a_day.out > multiple_times.out THEN 'once a day'
+		WHEN once_a_day.out < multiple_times.out THEN 'more than once a day'
+		WHEN once_a_day.out = multiple_times.out THEN 'both'
+	END
+FROM
+(
+SELECT DISTINCT outcome.point, dates.[date]
+FROM outcome
+CROSS APPLY
+(SELECT [date], point
+FROM outcome_o
+UNION SELECT [date], point
+FROM outcome)
+AS dates
+WHERE outcome.point = dates.point) AS left_side
+LEFT JOIN
+(SELECT SUM([out]) as [out], [date]
+FROM outcome
+GROUP BY [date]) AS multiple_times
+ON left_side.date = multiple_times.[date]
+LEFT JOIN
+(SELECT [out], [date]
+FROM outcome_o) AS once_a_day
+ON left_side.date = once_a_day.date
+
+/* Task 29 */
+
+SELECT 'B' As maker, 'pc' AS [type], model, price
+FROM pc
+WHERE model IN (SELECT model FROM product WHERE [type] = 'pc' AND maker = 'B')
+UNION ALL
+SELECT 'B' As maker, 'laptop' AS [type], model, price
+FROM laptop
+WHERE model IN (SELECT model FROM product WHERE [type] = 'laptop' AND maker = 'B')
+UNION ALL
+SELECT 'B' As maker, 'printer' AS [type], model, price
+FROM printer
+WHERE model IN (SELECT model FROM product WHERE [type] = 'printer' AND maker = 'B')
+
+/* Task 30 */
+z
+FROM ships
+WHERE [name] = class
+
+UNION
+
+SELECT ship, ship
+FROM outcomes
+WHERE ship IN (SELECT class FROM classes);
+
+/* Task 31 */
+
+SELECT class, COUNT([name])
+FROM ships
+GROUP BY class;
+
+/*Task 32 */
 
 SELECT [name]
 FROM ships
